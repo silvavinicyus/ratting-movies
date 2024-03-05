904c2d3d54ca385b9e15b7a55347244cb6ae9558
Vinicyus Silva <vinicyus346@gmail.com>
2024-03-05 19:52:19 -0300

    fix: fixing cors errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    @user = User.find_by(email: params[:email])
+    Rails.logger.info params
+    @user = User.find_by(email: params[:email])    
     if @user && @user.authenticate(params[:password])
       @token = encode_token(user_id: @user.id)
       render json: {
